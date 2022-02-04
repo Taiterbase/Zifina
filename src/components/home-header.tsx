eb5db94c519d25e5b0561e0af66d77fd00365fdb
Taite Nazifi <taitenazifi@gmail.com>
2022-02-04 08:50:35 -0700

    [components] pass props to header for title from layout

--- src/components/home-header.tsx
+++ src/components/home-header.tsx
@@ -1,21 +1,22 @@
 import { useRouter } from 'next/router'
 
-export default function HomeHeader(props: any) {
+export default function HomeHeader(props: { title: string }) {
+    const { title } = props;
     const router = useRouter();
 
     return (
         <div>
             <div className="ml-12 fixed h-16 max-h-16 min-w-full z-20 flex justify-between border-b-2 border-gray-400 border-opacity-20">
                 <div className="flex w-full place-items-center justify-evenly backdrop-filter backdrop-blur-lg">
-                    <p className="text-3xl text-slate-600">{`Next.js Website`}</p>
+                    <p className="text-3xl text-slate-600">{title}</p>
                 </div>
             </div>
             <div key="header" className="z-30 fixed overflow-x-hidden h-full w-20 flex-col justify-around list-content backdrop-filter backdrop-blur-3xl">
                 <div className="h-full">
-                    <button title="Home" onClick={() => { router.push("/") }} className="w-20 h-16 hover:bg-green-600 hover:bg-opacity-30">
+                    <button title="Home" onClick={() => { router.push("/") }} className="w-20 h-16 bg-green-600 bg-opacity-20 hover:bg-green-600 hover:bg-opacity-30">
                         <p>Link 1</p>
                     </button>
-                    <button title="Placeholder" onClick={() => { router.push("/") }} className="w-20 h-16 hover:bg-green-600 hover:bg-opacity-30">
+                    <button title="Placeholder" onClick={() => { router.push("/") }} className="w-20 h-16 bg-green-600 bg-opacity-20 hover:bg-green-600 hover:bg-opacity-30">
                         <p>Link 2</p>
                     </button>
                 </div>
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -8,7 +8,7 @@ export function HomeLayout(props: { children: ReactNode; }) {
 
     return (
         <div className="min-h-full min-w-full bg-blue-150">
-            <HomeHeader />
+            <HomeHeader title="My Website" />
             <main className="pl-28 overflow-y-auto p-6 pt-20 min-h-screen min-w-full flex justify-center flex-grow">
                 {children}
             </main>
